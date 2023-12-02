94943f29461032f22bee1bba0c4c9236da783372
Aun Muhammad <92050402+aunmuhammad6307@users.noreply.github.com>
2023-12-02 22:24:12 +0500

    import

--- server.js
+++ server.js
@@ -1,43 +1,17 @@
-const router = require('./routes/auth-routes');
-const mongoose = require('mongoose');
-const app = express();
-const dotenv = require('dotenv');
+import router from './routes/index.js';
+import mongoose from 'mongoose';
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env' });
-// const cookieParser = require('cookie-parser');
-// const cors = require('cors');
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+const app = express();
 
-// const app = express();
-// app.use(cookieParser());
-// app.use(express.json());
-// app.use(cors());
-// app.use(router);
-
-// const mongooseurl = process.env.MONGODB_URI;
-
-
-app.use(bodyParser.json());
-
-const mongooseurl = process.env.MONGODB_URI;
-
-mongoose.connect(mongooseurl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-mongoose.connection.on('connected', () => {
-    console.log("Connected to mongo");
-});
-
-mongoose.connection.on('error', (err) => {
-    console.log("Error connecting", err);
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-    res.end();
-    }
-);
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
 const uri = mongoose
   .connect(process.env.MONGODB_URI)
