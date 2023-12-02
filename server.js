41d0372e605ce77db7576505ae4fe49d940d2c2f
Aun Muhammad <92050402+aunmuhammad6307@users.noreply.github.com>
2023-12-02 22:08:25 +0500

    check

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
 const router = require('./routes/auth-routes');
 const mongoose = require('mongoose');
-const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env' });
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+// const cookieParser = require('cookie-parser');
+// const cors = require('cors');
 
-const app = express();
-app.use(cookieParser());
-app.use(express.json());
-app.use(cors());
-app.use(router);
+// const app = express();
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use(cors());
+// app.use(router);
 
-const mongooseurl = process.env.MONGODB_URI;
+// const mongooseurl = process.env.MONGODB_URI;
 
+
+app.use(bodyParser.json());
+
+const mongooseurl = 'mongodb+srv://physicsbook:VyjXsryB2p8VRrgX@cluster0.pjsmtiy.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.connect(mongooseurl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 mongoose.connection.on('connected', () => {
-  console.log("Connected to mongo");
+    console.log("Connected to mongo");
 });
 
 mongoose.connection.on('error', (err) => {
-  console.log("Error connecting", err);
+    console.log("Error connecting", err);
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-  res.end();
-})
+    res.send('Hello World');
+    res.end();
+    }
+);
 
 const uri = mongoose
   .connect(process.env.MONGODB_URI)
